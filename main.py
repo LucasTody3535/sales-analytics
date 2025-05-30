56b40b0caec84808939968206462802838aa3d28
Toodie <lucas.vtodt@gmail.com>
2025-05-30 14:33:49 -0300

    feat(report): generate report with initial message

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
+from src.sales_dataset import SalesDataset
+from src.report import Report
+
 if __name__ == "__main__":
-    print("Hello World!")
+    sales = SalesDataset("data/Sales Dataset.csv")
+    report = Report("A4", "Portrait")
+    templates_content = None
+
+    sales.extract_rows()
+    report.init("Sales Report for the XPTO Company")
+
+    with open("templates/greetings.txt") as greetings:
+        content = greetings.read()
+        content = content.replace("<t1>", "John Doe")
+        content = content.replace("<t2>", "XPTO")
+
+        font_data = { "family": "Times", "style": "", "size": 12 }
+        font_coord = { "x": report.dims()["left_m"], "y": 15 }
+        report.add_text(content, font_coord, font_data, multiline=True)
+
+    report.gen("out/Report.pdf")
