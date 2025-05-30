3ffe8f657f11f653a805debbf3785c222e9a324a
Toodie <lucas.vtodt@gmail.com>
2025-05-30 13:45:37 -0300

    feat(core): impl class SalesDatasetColumn

---
+++ src/sales_dataset_column.py
@@ -0,0 +1,23 @@
+class SalesDatasetColumn:
+    """
+        Represents a column in the sales dataset, composed by an identifier related
+        to the column and the corresponding cells
+    """
+    def __init__(self, name: str):
+        self.set_name(name)
+        self.set_cells([])
+
+    def set_name(self, name: str):
+        self.__name = name
+
+    def get_name(self) -> str:
+        return self.__name
+
+    def set_cells(self, cells: list[str]):
+        self.__cells = cells
+
+    def get_cells(self) -> list[str]:
+        return self.__cells.copy()[1:]
+
+    def append_cell_value(self, value: str):
+        self.__cells.append(value)
