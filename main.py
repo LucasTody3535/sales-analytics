be85f4ebf9a2c7320d79f5d6adc9b56bcd654cbe
Toodie <lucas.vtodt@gmail.com>
2025-06-17 11:19:25 -0300

    feat(report): add categories profit chart to report

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from os import remove
+
 from src.sales_dataset import SalesDataset
 from src.report import Report
 from src.barchart import BarChart
@@ -39,5 +41,25 @@ if __name__ == "__main__":
     bar_chart.set_data(categories_profits)
     bar_chart.set_colors(categories_colors)
     bar_chart.gen_chart(title="Total profit")
+
+    categories_chart_image_path = "./out/categories_profit.png"
+    most_profitable_category_idx = categories_profits.index(max(categories_profits))
+    least_profitable_category_idx = categories_profits.index(min(categories_profits))
+
+    with open("templates/categories.txt", encoding="utf-8") as categories_intro:
+        content = categories_intro.read()
+        content = content.replace("<c1>", categories_names[most_profitable_category_idx].lower())
+        content = content.replace("<c2>", categories_names[least_profitable_category_idx].lower())
+
+        font_data = { "family": "Times", "style": "", "size": 12 }
+        font_coord = { "x": report.dims()["left_m"], "y": 30 }
+        report.add_text(content, font_coord, font_data, multiline=True)
+
+    bar_chart.save_as_image(categories_chart_image_path)
+    img_dims = { "height": 110, "width": 180 }
+    img_coord = { "x": report.dims()["left_m"] + 13, "y": 40 }
+    report.add_image(categories_chart_image_path, img_dims, img_coord)
+    
+    remove(categories_chart_image_path)
 
     report.gen("out/Report.pdf")
