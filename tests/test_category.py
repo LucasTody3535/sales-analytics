086d8f8925459a9e11d608819f738eb39ede3aec
Toodie <lucas.vtodt@gmail.com>
2025-07-17 02:57:54 -0300

    test: add unit test for category profit extraction
    This test covers both category and subcategories profit extraction

--- tests/test_category.py
+++ tests/test_category.py
@@ -25,3 +25,26 @@ class TestCategoryClass(TestCase):
             expected_color,
             f"Color extraction returns incorrect color for category: {category_color}!\nExpected value: {expected_color}",
         )
+
+    def test_correctness_of_profits_extraction_behavior(self):
+        expected_subcategories_profits = [33.4, 44.32, 10.02]
+        expected_category_profit = sum(expected_subcategories_profits)
+        category = Category("Office", expected_category_profit, "red", [])
+        subcategories = [
+            Category("Paper", expected_subcategories_profits[0], "red", []),
+            Category("Notebook", expected_subcategories_profits[1], "red", []),
+            Category("Pens", expected_subcategories_profits[2], "red", []),
+        ]
+        category.subcategories = subcategories
+        category_profit, subcategories_profits = category.extract_profits()
+        self.assertEqual(
+            subcategories_profits,
+            expected_subcategories_profits,
+            f"Profit extraction returns unexpected values for subcategories: {subcategories_profits}!\n"
+            + f"Expected values: {expected_subcategories_profits}!",
+        )
+        self.assertEqual(
+            category_profit,
+            expected_category_profit,
+            f"Profit extraction returns incorrect profit for category: {category_profit}!\nExpected value: {expected_category_profit}",
+        )
